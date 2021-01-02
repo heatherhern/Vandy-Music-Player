3d3009ca240245b0191e5badff75fe47764e9ce0
Caleb Chaney <calebc347@gmail.com>
2021-01-02 11:38:31 -0600

    Updated controller, model, and package.json

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,8 @@
 const db = require("../models");
 const axios = require("axios");
-const request = require('request'); 
-let client_id = 'fa815dbc6eeb4d67ac7eceac01a7caa5'; 
-let client_secret = 'a32a2cb1e69d428dba7a141d61d21e30'; 
+const request = require('request');
+let client_id = 'fa815dbc6eeb4d67ac7eceac01a7caa5';
+let client_secret = 'a32a2cb1e69d428dba7a141d61d21e30';
 
 module.exports = {
 
@@ -12,39 +12,6 @@ module.exports = {
             .then((response) => res.json(response.data))
             .catch(err => res.status(422).json(err))
     },
-}
-
-// your application requests authorization
-var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-    },
-    form: {
-        grant_type: 'client_credentials'
-    },
-    json: true
-};
-
-request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-
-        // use the access token to access the Spotify Web API
-        var token = body.access_token;
-        var options = {
-            url: 'https://api.spotify.com/v1/users/jmperezperez',
-            headers: {
-                'Authorization': 'Bearer ' + token
-            },
-            json: true
-        };
-        request.get(options, function (error, response, body) {
-            console.log(body);
-        });
-    }
-});
-
-// GET https://api.spotify.com/v1/search
 
     // Find All Songs in Database
     findAll: (req, res) => {
@@ -72,3 +39,35 @@ request.post(authOptions, function (error, response, body) {
     },
 };
 
+
+// your application requests authorization
+// var authOptions = {
+//     url: 'https://accounts.spotify.com/api/token',
+//     headers: {
+//         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+//     },
+//     form: {
+//         grant_type: 'client_credentials'
+//     },
+//     json: true
+// };
+
+// request.post(authOptions, function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+
+//         // use the access token to access the Spotify Web API
+//         var token = body.access_token;
+//         var options = {
+//             url: 'https://api.spotify.com/v1/users/jmperezperez',
+//             headers: {
+//                 'Authorization': 'Bearer ' + token
+//             },
+//             json: true
+//         };
+//         request.get(options, function (error, response, body) {
+//             console.log(body);
+//         });
+//     }
+// });
+
+// GET https://api.spotify.com/v1/search
