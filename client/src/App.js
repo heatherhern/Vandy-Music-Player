d6e929a0183bcbb285b2d9b523ff3027af3ff621
Jonathan Stillman <jonathanstillman86@yahoo.com>
2021-01-07 15:57:21 -0500

    App.js Edit
    App.js Edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateAccount from "./components/CreateAccount";
-import Search from "./components/Search/Search";
-import SearchResults from "./components/SearchResults/SearchResults";
 import Login from "./components/Login";
 
 
@@ -16,10 +14,6 @@ function App() {
           </Route>
           <Route exact path="/login">
             <Login />
-          </Route>
-          <Route exact path="/search">
-            <Search />
-            <SearchResults />
           </Route>
         </Switch>
       </div>
