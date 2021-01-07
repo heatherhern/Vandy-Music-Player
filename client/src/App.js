fceb1454370a7dec869f9aa119f78b99b1af7713
Heather Hern <heathernewlin1@gmail.com>
2021-01-06 20:55:13 -0500

    Added search route + search box styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateAccount from "./components/CreateAccount";
 // import Login from "./components/Login";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           {/* <Route exact path="/login">
             <Login />
           </Route> */}
+          <Route exact path="/search">
+            <Search />
+          </Route>
         </Switch>
       </div>
     </Router>
--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./search.css";
+import "./Search.css";
 
 function Search(props) {
     return (
