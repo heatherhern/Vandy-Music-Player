5a7acfdebb344fa15c6f5b90b3176914991aa8da
Caleb Chaney <calebc347@gmail.com>
2021-01-09 11:56:04 -0600

    Small change to userAuth model

--- models/userAuth.js
+++ models/userAuth.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
-const userAuthSchema= new Schema({
+const userAuthSchema = new Schema({
     firstName:  { type: String, required: true },
     lastName:   { type: String, required: true },
     email:  { type: String, required: true },
