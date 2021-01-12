5ebee1f2ac7823ff2cee79d77d15a73a846e99dc
Heather Hern <heathernewlin1@gmail.com>
2021-01-11 20:08:43 -0500

    Working on API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,44 +1,57 @@
 import axios from "axios";
 import searchQuery from "../components/Search/";
+require('dotenv').config()
 
+
+// http://ws.audioscrobbler.com/2.0/?
+// method=artist.gettoptracks
+// &artist=
+// cher
+// &api_key=bf8d3ec29d790d8fdc71b82b15ad96a3&format=json
 
 // Export an object containing methods we'll use for accessing the playlist.
+
+
 export default {
     APICall: function (query) {
-        return axios.get("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + query)
-    },
-
-    // Gets all Songs
-    getSongs: function () {
-        return axios.get("/api/songs");
-    },
-
-    // Gets a Specific Song 
-    getSong: function (id) {
-        return axios.get("/api/songs/" + id);
-    },
-
-    // Saves a Song to the Database
-    saveSong: function (savedSongs) {
-        return axios.post("/api/songs", savedSongs);
-    },
-
-    // Deletes a Song
-    deleteSong: function (id) {
-        return axios.delete("/api/songs/" + id);
-    },
- 
-    getAlbum: function (query) {
-        return axios.get("https://api.deezer.com/album/302127" + searchQuery);
-    },
-  
-    getArtist: function () {
-        return axios.get("https://api.deezer.com/artist/" + searchQuery);
-    },
-      
-    getChart: function () {
-        return axios.get("https://api.deezer.com/chart/" + searchQuery);
+        return axios.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + searchQuery + "&api_key=" + process.env.api_key + "&format=json");
+        // console.log(res);
     }
-};
+}
+
+    // ,
+
+//     // Gets all Songs
+//     getSongs: function () {
+//         return axios.get("/api/songs");
+//     },
+
+//     // Gets a Specific Song 
+//     getSong: function (id) {
+//         return axios.get("/api/songs/" + id);
+//     },
+
+//     // Saves a Song to the Database
+//     saveSong: function (savedSongs) {
+//         return axios.post("/api/songs", savedSongs);
+//     },
+
+//     // Deletes a Song
+//     deleteSong: function (id) {
+//         return axios.delete("/api/songs/" + id);
+//     },
+
+//     getAlbum: function (query) {
+//         return axios.get("https://api.deezer.com/album/302127" + searchQuery);
+//     },
+
+//     getArtist: function () {
+//         return axios.get("https://api.deezer.com/artist/" + searchQuery);
+//     },
+
+//     getChart: function () {
+//         return axios.get("https://api.deezer.com/chart/" + searchQuery);
+//     }
+// };
 
 
